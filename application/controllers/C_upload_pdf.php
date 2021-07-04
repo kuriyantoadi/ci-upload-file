0ab9f0219b99011bfee2a1677a6485607d7be125
kuriyantoadi <melatikuning212@gmail.com>
2021-07-04 16:22:21 +0700

    update 4 juli 2021, update 3

--- application/controllers/C_upload_pdf.php
+++ application/controllers/C_upload_pdf.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_upload extends CI_Controller {
+class C_upload_pdf extends CI_Controller {
 
   function __construct(){
     parent::__construct();
